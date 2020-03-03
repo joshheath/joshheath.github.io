39631ec3a73d117ceb8d5aa9a133453e9efe63c4
Josh Heath <37807868+joshheath@users.noreply.github.com>
2020-03-03 15:30:21 -0500

    add sentContact function to contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,35 @@
-var fields = {
-  document.addEventListener("DOMContentLoaded", function(){
-    fields.lastname = document.getElementById('lastname');
-    fields.firstname = document.getElementById('firstname');
-    fields.email = document.getElementById('email');
-    fields.question = document.getElementById('question');
-  })
-};
+var fields = {};
+
+class User {
+  constructor(firstname, lastname, email){
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.question = question;
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  fields.lastname = document.getElementById('lastname');
+  fields.firstname = document.getElementById('firstname');
+  fields.email = document.getElementById('email');
+  fields.question = document.getElementById('question');
+})
+
 
 function isNotEmpty(value){
   if(value == null || typeof value == 'undefined' ) return false;
   return (value.length > 0);
 }
 
+
 function isEmail(email){
   let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return regex.test(String(email).toLowerCase());
 }
+
 
 function fieldValidation(field, validationFunction){
   if (field == null) return false;
@@ -30,13 +43,27 @@ function fieldValidation(field, validationFunction){
   return isFieldValid;
 }
 
+
 function isValid(){
   var valid = true;
 
-  valid &= fieldValidation(fields.last)
+  valid &= fieldValidation(fields.lastname, isNotEmpty);
+  valid &= fieldValidation(fields.firstname, isNotEmpty);
+  valid &= fieldValidation(fields.email, isNotEmpty);
 
+  return valid;
 }
 
+
+function sendContact(){
+  if(isValid()){
+    let usr = new User(firstname.value, lastname.value, email.value, question.value);
+
+    alert(`${usr.firstname} thanks for getting in touch!`)
+  } else {
+    alert("There was an error")
+  }
+}
 
 
 
