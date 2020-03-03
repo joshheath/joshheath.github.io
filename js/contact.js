74b505490b91994b40f66de59a445ac1d521b181
Josh Heath <37807868+joshheath@users.noreply.github.com>
2020-03-03 14:35:47 -0500

    add js isnotempty and regex

--- js/contact.js
+++ js/contact.js
@@ -2,3 +2,23 @@ function textBox(){
   var x = document.getElementById('myText').value;
   document.getElementById("demo").innerHTML = x;
 };
+
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function(){
+  fields.lastname = document.getElementById('lastname');
+  fields.firstname = document.getElementById('firstname');
+  fields.email = document.getElementById('email');
+  fields.question = document.getElementById('question');
+})
+
+function isNotEmpty(value){
+  if(value == null || typeof value == 'undefined' ) return false;
+  return (value.length > 0);
+}
+
+function isEmail(email){
+  let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return regex.test(String(email).toLowerCase());
+}
